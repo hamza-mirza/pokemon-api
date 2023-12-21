f788f1fae5d887bb9d536b79e2c2c8e103126f17
Hamza Shahid <hammzzeeee@gmail.com>
2023-12-21 03:27:54 +0000

    Housekeeping: Clean up redundant code, make new components for pokemon cards & Search

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,20 +1,40 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 const Modal = ({ isOpen, onClose, children }) => {
+  const [isDisplaying, setIsDisplaying] = useState(isOpen)
+
   useEffect(() => {
+    let timeoutId
     if (isOpen) {
-      document.body.style.overflow = 'hidden'
+      setIsDisplaying(true)
+    } else {
+      timeoutId = setTimeout(() => {
+        setIsDisplaying(false)
+      }, 500)
     }
-    return () => {
+
+    return () => clearTimeout(timeoutId)
+  }, [isOpen])
+
+  useEffect(() => {
+    if (isOpen || isDisplaying) {
+      document.body.style.overflow = 'hidden'
+    } else {
       document.body.style.overflow = 'unset'
     }
-  }, [isOpen])
-  if (!isOpen) return null
+  }, [isOpen, isDisplaying])
 
+  if (!isDisplaying) return null
+
+  const modalClass = isOpen ? 'modal--is-open' : 'modal--is-closed'
+  const backdropClass = isOpen ? 'modal-backdrop--is-open' : 'modal-backdrop--is-closed'
   return (
     <>
-      <div className="modal-backdrop"></div>
-      <div className="modal">
+      <div
+        className={`modal-backdrop ${backdropClass}`}
+        onClick={onClose}
+      ></div>
+      <div className={`modal ${modalClass}`}>
         <div className="modal-content">
           <button
             className="modal-close"
